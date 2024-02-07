71437e95f50c3b87a4df51889cc3c01a59e0c119
fenil1312 <patelfenil578@gmail.com>
2024-02-07 14:04:21 +0530

    Java Project

---
+++ MiniProject/src/hotelmanagement/hotel_manage.sql
@@ -0,0 +1,9 @@
+/*
+-- Query: DESCRIBE hotel_manage
+-- Date: 2024-01-27 14:00
+*/
+INSERT INTO `` (`Field`,`Type`,`Null`,`Key`,`Default`,`Extra`) VALUES ('reservation_id','int','NO','PRI',NULL,'auto_increment');
+INSERT INTO `` (`Field`,`Type`,`Null`,`Key`,`Default`,`Extra`) VALUES ('guest_name','varchar(255)','NO','',NULL,'');
+INSERT INTO `` (`Field`,`Type`,`Null`,`Key`,`Default`,`Extra`) VALUES ('room_number','int','NO','',NULL,'');
+INSERT INTO `` (`Field`,`Type`,`Null`,`Key`,`Default`,`Extra`) VALUES ('contact_number','varchar(10)','NO','',NULL,'');
+INSERT INTO `` (`Field`,`Type`,`Null`,`Key`,`Default`,`Extra`) VALUES ('reservation_date','timestamp','YES','','CURRENT_TIMESTAMP','DEFAULT_GENERATED');
